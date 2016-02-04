28191d93a670079cd78b79cbc50b5427154e1d20
Cory Ribson <Cory Ribson>
2016-02-04 15:11:43 -0800

    Includes css-loader and style-loader in webpack.config for active reloading on style changes

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,17 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegEx! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths
+        include: PATHS.app
+      }
+    ]
   }
 };
 
