b34adeaad82bc1c0e91443b24200f50099400496
Cory Ribson <Cory Ribson>
2016-02-03 19:34:43 -0800

    Adds npm, webpack, and assets for Hello World components

---
+++ app/component.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+  var element = document.createElement('hi');
+
+  element.innerHTML = 'Hello World1';
+
+  return element;
+};
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'build.js'
+  }
+};
