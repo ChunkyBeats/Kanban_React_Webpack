8b8e8a7a5fa43952c37c8627be21646c20f5c746
Cory Ribson <Cory Ribson>
2016-02-04 14:35:38 -0800

    Modifies webpack.config.js to define split points for npm script customization

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries
   entry: PATHS.app,
   output: {
@@ -13,3 +15,12 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default Configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
