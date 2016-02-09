895bde2e91414efce0619cf535e8458271bba374
Cory Ribson <crbi2550@gmail.com>
2016-02-08 17:43:07 -0800

    Notes now contained in the state of the app to make modifiable

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
+// import Note from './Note.jsx';
 
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Laundry'
+        }
+      ]
+    };
   }
+
+  render() {
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <ul>{notes.map(note =>
+          <li key={note.id}>{note.task}</li>
+        )}</ul>
+      </div>
+    );
+  }
+
+  // Property Initializer - binds "this" to point to App
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    });
+  };
+
 }
