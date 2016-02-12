d3adc02256dc995e1deecc22882594b2e6987712
Cory Ribson <crbi2550@gmail.com>
2016-02-11 17:21:07 -0800

    Adds Lanes to app.  CRUD functionality coded, but NoteStore is SINGLETON so notes are shared to all lanes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,37 +1,29 @@
 import AltContainer from 'alt-container';
 import React from 'react';
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
+        <button className="add-lane" onClick={this.addLane}>+</button>
         <AltContainer
-          stores={[NoteStore]}
+          stores={[LaneStore]}
           inject={{
-            notes: () => NoteStore.getState().notes
+            lanes: () => LaneStore.getState().lanes || []
           }} >
-          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+          <Lanes />
         </AltContainer>
       </div>
     );
   }
 
-  // Property Initializer - binds "this" to point to App
-  addNote() {
-    NoteActions.create({task: 'New Task'});
-  }
-
-  editNote(id, task) {
-    NoteActions.update({id, task});
-  }
-
-  deleteNote(id) {
-    NoteActions.delete(id);
+  addLane() {
+    LaneActions.create({name: "New Lane"});
   }
 
 }
