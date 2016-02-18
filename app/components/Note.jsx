4bce3494a4c330e86ad490a071af7682259ebd23
Cory Ribson <crbi2550@gmail.com>
2016-02-18 15:57:04 -0800

    Implements isDragging for opacity change on selection

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -8,6 +8,9 @@ const noteSource = {
     return{
       id: props.id
     };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
   }
 };
 
@@ -23,8 +26,9 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()  // Map isDragging state to isDragging prop
 }))
 
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
@@ -32,11 +36,12 @@ const noteTarget = {
 }))
 export default class Note extends React.Component {
   render() {
-    const {connectDragSource, connectDropTarget,
+    const {connectDragSource, connectDropTarget, isDragging,
       id, onMove, ...props} = this.props;
 
     return connectDragSource(connectDropTarget(
-      <li {...props}>{props.children}</li>
+      <li style={{opacity: isDragging ? 0 : 1}}
+      {...props}>{props.children}</li>
     ));
   }
 }
